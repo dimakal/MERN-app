0cf0e98095b20a6efee4d78a266b4a1a849b61f1
dimakal <dimakal94@mail.ru>
2021-03-26 16:15:35 +0300

    add dotenv using example

--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ import bodyParser from  'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
-const CONNECTION_URL = `mongodb+srv://dimakal:test@cluster0.lmkez.mongodb.net/myFirstDatabase`
-const PORT = process.env.PORT || 3011
+// import dotenv from 'dotenv'
 
 const app = express()
+// dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+
+const CONNECTION_URL = `mongodb+srv://dimakal:test@cluster0.lmkez.mongodb.net/myFirstDatabase`
+const PORT = process.env.PORT || 3011
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true,
